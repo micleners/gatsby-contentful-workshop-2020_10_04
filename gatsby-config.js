9f31a4074bad332625a59a533cb7135e60aef72a
Michael Leners <micleners@gmail.com>
2020-10-08 16:07:34 -0500

    Add organizers from JSON

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,11 +7,19 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-styled-components`,
+    `gatsby-transformer-json`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
         path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `static`,
+        path: `${__dirname}/src/static`,
       },
     },
     `gatsby-transformer-sharp`,
--- src/pages/organizers.js
+++ src/pages/organizers.js
@@ -1,14 +1,53 @@
 import React from "react"
-import { Link } from "gatsby"
+import { useStaticQuery, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { Flex, Box, Image, Heading, Link } from "rebass"
 
-const Organizers = () => (
-  <Layout>
-    <SEO title="Organizers" />
-    <h1>Organizers</h1>
-  </Layout>
-)
+const Organizers = () => {
+  const data = useStaticQuery(graphql`
+    {
+      organizers: allOrganizersJson {
+        edges {
+          node {
+            email
+            employer
+            name
+            id
+            phone
+            photo
+            role
+            website
+          }
+        }
+      }
+    }
+  `)
+
+  const organizers = data.organizers.edges.map(org => org.node)
+
+  return (
+    <Layout>
+      <SEO title="Organizers" />
+      <h1>Organizers</h1>
+      {organizers.map(organizer => (
+        <Flex key={organizer.id}>
+          <Box mb="30px">
+            <Image src={organizer.photo} />
+            <Heading fontSize="4">{organizer.name}</Heading>
+          </Box>
+          <Box ml="20px">
+            <Box>Role: {organizer.role}</Box>
+            <Box>Phone: {organizer.phone}</Box>
+            <Box>Email: {organizer.email}</Box>
+            <Box>Workplace: {organizer.employer}</Box>
+            {organizer.website && <Link href={organizer.website}>Website</Link>}
+          </Box>
+        </Flex>
+      ))}
+    </Layout>
+  )
+}
 
 export default Organizers
